2ce75630eb8971c591d087f3295f3640c2c53c10
MaromB <maromblum@gmail.com>
2023-12-07 18:55:07 +0200

    Initial Commit

---
+++ BlumenfeldCartApp/BlumenfeldCartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlumenfeldCartAppApp.swift
+//  BlumenfeldCartApp
+//
+//  Created by מרום בלומנפלד on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlumenfeldCartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
